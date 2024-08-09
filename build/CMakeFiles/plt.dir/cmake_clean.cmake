bfaf2e2c2714469e93f5002bfd46564e6326c895
kaitoyamazaki <reflect018@yahoo.co.jp>
2024-08-09 16:51:40 +0900

    matplotlibcppのtest

---
+++ build/CMakeFiles/plt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plt.dir/main.cpp.o"
+  "CMakeFiles/plt.dir/main.cpp.o.d"
+  "plt"
+  "plt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
